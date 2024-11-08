6f4d39fbfe7e49b9131e7fa23354d4fcfee9ffb1
Bhamidipati Sarma <abhinavsarma245@gmail.com>
2024-11-08 15:53:15 +0530

    adding project

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const env= require('dotenv');
+env.config();
+const port = process.env.port||5000;
+
+app.listen(port,()=>{
+    console.log(`server is running in${port}`);
+
+});
+
+app.get("/bot",(req,res)=>{
+    res.send("gayatri ni dengu");
+})
